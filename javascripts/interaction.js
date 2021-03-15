b492f5aa9455d1294bf9fefcc855bebf1139ad65
bymhlkv <by.mhlkv@gmail.com>
2021-03-16 01:10:10 +0300

    Update interaction.js

--- javascripts/interaction.js
+++ javascripts/interaction.js
@@ -1,1 +1,14 @@
+$( "#draggable" ).draggable();
+$( "#draggable" ).click(function() {
+  console.log('clicked!')
+});
 
+
+$(function() {
+  $(".circle1a, .circle2a, .circle3a, .circle4a, .circle5a, .circle6a, .circle7a, .circle8a, .circle9a, .circle10a, .circle11a, .circle12a, .circle13a, .circle14a, .circle15a, .circle16a, .circle17a, .circle18a, .circle19a, .rectangle1a, .rectangle2a, .rectangle3a, .rectangle4a, .rectangle5a, .rectangle6a, .colorimg1a, .colorimg2a, .colorimg3a, .colorimg4a, .whiteimg1, .whiteimg2, .cross1a, .cross2a, .cross3a, .cross4a, .cross5a, .cross6a, .cross7a, .cross8a, .cross9a, .figimg1a, .figimg2a, .figimg3a").draggable();
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+
+      <script src="javascripts/interaction.js" charset="utf-8"></script>
