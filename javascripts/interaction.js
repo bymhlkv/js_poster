d88b698369a681cfd5c393369b934c353828334a
bymhlkv <by.mhlkv@gmail.com>
2021-03-16 15:06:23 +0300

    Update interaction.js

--- javascripts/interaction.js
+++ javascripts/interaction.js
@@ -2,6 +2,10 @@ $(function() {
   $(".circle1a, .circle2a, .circle3a, .circle4a, .circle5a, .circle6a, .circle7a, .circle8a, .circle9a, .circle10a, .circle11a, .circle12a, .circle13a, .circle14a, .circle15a, .circle16a, .circle17a, .circle18a, .circle19a, .rectangle1a, .rectangle2a, .rectangle3a, .rectangle4a, .rectangle5a, .rectangle6a, .colorimg1a, .colorimg2a, .colorimg3a, .colorimg4a, .whiteimg1, .whiteimg2, .cross1a, .cross2a, .cross3a, .cross4a, .cross5a, .cross6a, .cross7a, .cross8a, .cross9a, .figimg1a, .figimg2a, .figimg3a").draggable();
 });
 
+$(function() {
+  $(".circle1, .circle2, .circle3, .circle4, .circle5, .circle6, .circle7, .circle8, .circle9, .circle10, .circle11, .circle12, .circle13, .circle14, .circle15, .circle16, .circle117, .rectangle1, .rectangle2, .rectangle3, .colorimg1, .colorimg2, .colorimg3, .whiteimg, .cross1, .cross2, .cross3, .cross4, .cross5, .cross6, .cross7, .cross8, .figimg1, .figimg2, .figimg3").draggable();
+});
+
 
 $(document).ready(function(){
   $('#screen3_1').hide();
