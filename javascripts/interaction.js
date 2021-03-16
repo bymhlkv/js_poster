4cd6453b1acd0bc6492e06e34c3d3bec9981086d
bymhlkv <by.mhlkv@gmail.com>
2021-03-16 13:04:22 +0300

    Update interaction.js

--- javascripts/interaction.js
+++ javascripts/interaction.js
@@ -1,14 +1,66 @@
-$( "#draggable" ).draggable();
-$( "#draggable" ).click(function() {
-  console.log('clicked!')
+$(function() {
+  $(".circle1a, .circle2a, .circle3a, .circle4a, .circle5a, .circle6a, .circle7a, .circle8a, .circle9a, .circle10a, .circle11a, .circle12a, .circle13a, .circle14a, .circle15a, .circle16a, .circle17a, .circle18a, .circle19a, .rectangle1a, .rectangle2a, .rectangle3a, .rectangle4a, .rectangle5a, .rectangle6a, .colorimg1a, .colorimg2a, .colorimg3a, .colorimg4a, .whiteimg1, .whiteimg2, .cross1a, .cross2a, .cross3a, .cross4a, .cross5a, .cross6a, .cross7a, .cross8a, .cross9a, .figimg1a, .figimg2a, .figimg3a").draggable();
 });
 
 
-$(function() {
-  $(".circle1a, .circle2a, .circle3a, .circle4a, .circle5a, .circle6a, .circle7a, .circle8a, .circle9a, .circle10a, .circle11a, .circle12a, .circle13a, .circle14a, .circle15a, .circle16a, .circle17a, .circle18a, .circle19a, .rectangle1a, .rectangle2a, .rectangle3a, .rectangle4a, .rectangle5a, .rectangle6a, .colorimg1a, .colorimg2a, .colorimg3a, .colorimg4a, .whiteimg1, .whiteimg2, .cross1a, .cross2a, .cross3a, .cross4a, .cross5a, .cross6a, .cross7a, .cross8a, .cross9a, .figimg1a, .figimg2a, .figimg3a").draggable();
+$(document).ready(function(){
+  $('#screen3_1').hide();
+});
+$(document).ready(function(){
+  $('#screen3_2').hide();
+});
+$(document).ready(function(){
+  $('#screen3_3').hide();
+});
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
 
-      <script src="javascripts/interaction.js" charset="utf-8"></script>
+$(document).ready(function(){
+  $('#bubble1').hide();
+});
+$(document).ready(function(){
+  $('#bubble2').hide();
+});
+$(document).ready(function(){
+  $('#bubble3').hide();
+});
+$(document).ready(function(){
+  $('#bubble4').hide();
+});
+$(document).ready(function(){
+  $('#bubble5').hide();
+});
+
+
+$(document).ready(function(){
+  $("#helpimg1a").on('click', function(){
+    $('#screen3_2').show();
+  });
+});
+$(document).ready(function(){
+  $("#okbotton_main").on('click', function(){
+    $('#screen3_2').hide();
+  });
+});
+
+
+
+$(document).ready(function(){
+  $("#cross5a").on('click', function(){
+    $('#bubble1').toggle();
+  });
+});
+$(document).ready(function(){
+  $("#cross8a").on('click', function(){
+    $('#bubble2').toggle();
+  });
+});
+$(document).ready(function(){
+  $("#cross9a").on('click', function(){
+    $('#bubble3').toggle();
+  });
+});
+// $(document).ready(function(){
+//   $("#cross9a").on('click', function(){
+//     $('#bubble3').toggle();
+//   });
+// });
